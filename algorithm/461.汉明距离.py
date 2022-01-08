184aee0126ceffc94d80e0d6c87d9220c48d308c
spriteboysz <sprite_bx@126.com>
2022-01-08 15:32:48 +0800

    2022010815

---
+++ algorithm/461.汉明距离.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: Deean
+Date: 2022-01-08 14:39:43
+LastEditTime: 2022-01-08 14:57:35
+Description: 
+FilePath: 461.汉明距离.py
+'''
+#
+# @lc app=leetcode.cn id=461 lang=python3
+#
+# [461] 汉明距离
+#
+
+# @lc code=start
+
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        bx, by = bin(x)[2:], bin(y)[2:]
+        if len(bx) > len(by):
+            bx, by = by, bx
+        bx = "0" * (len(by) - len(bx)) + bx
+        count = 0
+        for i, j in zip(bx, by):
+            if int(i) ^ int(j) == 1:
+                count += 1
+        return count
+
+
+# @lc code=end
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.hammingDistance(10, 1))
---
+++ algorithm/463.岛屿的周长.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: Deean
+Date: 2022-01-08 15:12:23
+LastEditTime: 2022-01-08 15:28:52
+Description: 
+FilePath: 463.岛屿的周长.py
+'''
+#
+# @lc app=leetcode.cn id=463 lang=python3
+#
+# [463] 岛屿的周长
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        n, m = len(grid), len(grid[0])
+        matrix = [[0] * (m + 2) for _ in range(n + 2)]
+        for i in range(1, n + 1):
+            matrix[i][1:m + 1] = grid[i - 1]
+
+        count = 0
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if matrix[i][j]:
+                    count += 4 - (matrix[i - 1][j] + matrix[i + 1]
+                                  [j] + matrix[i][j - 1] + matrix[i][j + 1])
+        return count
+# @lc code=end
+
+
+if __name__ == '__main__':
+    s = Solution()
+    # print(s.islandPerimeter(
+    #     [[0, 1, 0, 0], [1, 1, 1, 0], [0, 1, 0, 0], [1, 1, 0, 0]]))
+    # print(s.islandPerimeter([[1]]))
+    # print(s.islandPerimeter([[1, 0]]))
+    print(s.islandPerimeter([[0, 1]]))
