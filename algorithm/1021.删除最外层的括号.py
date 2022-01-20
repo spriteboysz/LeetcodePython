f1e32e9c7e3e00b760f227933643c268b0b4a291
spriteboysz <sprite_bx@126.com>
2022-01-21 00:22:48 +0800

    2022012100

---
+++ algorithm/1021.删除最外层的括号.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: Deean
+Date: 2022-01-21 00:01:29
+LastEditTime: 2022-01-21 00:22:36
+Description: 
+FilePath: 1021.删除最外层的括号.py
+'''
+#
+# @lc app=leetcode.cn id=1021 lang=python3
+#
+# [1021] 删除最外层的括号
+#
+
+# @lc code=start
+
+
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        primitive = []
+        start, count = 0, 0
+        for i, item in enumerate(s):
+            if item == "(":
+                count += 1
+            elif item == ")":
+                count -= 1
+            if count == 0:
+                primitive.append(s[start + 1:i])
+                start = i + 1      
+        return "".join(primitive)
+
+
+# @lc code=end
+if __name__ == '__main__':
+    s = Solution()
+    print(s.removeOuterParentheses("(()())(())(()(()))"))
+    print(s.removeOuterParentheses("()()"))
