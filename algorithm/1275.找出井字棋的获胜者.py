b8d96e6f252526d6f823710eef687fe1cb02c311
spriteboysz <sprite_bx@126.com>
2022-02-02 00:25:29 +0800

    2022020200

---
+++ algorithm/1275.找出井字棋的获胜者.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: Deean
+Date: 2022-02-01 23:57:49
+LastEditTime: 2022-02-02 00:24:02
+Description: 
+FilePath: 1275.找出井字棋的获胜者.py
+'''
+#
+# @lc app=leetcode.cn id=1275 lang=python3
+#
+# [1275] 找出井字棋的获胜者
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def check(self, chess):
+        if chess[0] == chess[1] == chess[2]:
+            return True
+        if chess[3] == chess[4] == chess[5]:
+            return True
+        if chess[6] == chess[7] == chess[8]:
+            return True
+        if chess[0] == chess[3] == chess[6]:
+            return True
+        if chess[1] == chess[4] == chess[7]:
+            return True
+        if chess[2] == chess[5] == chess[8]:
+            return True
+        if chess[0] == chess[4] == chess[8]:
+            return True
+        if chess[2] == chess[4] == chess[6]:
+            return True
+        else:
+            return False
+
+    def tictactoe(self, moves: List[List[int]]) -> str:
+        chess = [i for i in range(9)]
+        for i in range(0, len(moves), 2):
+            chess[moves[i][0] * 3 + moves[i][1]] = "A"
+            if self.check(chess):
+                return "A"
+            if i + 1 < len(moves):
+                chess[moves[i + 1][0] * 3 + moves[i + 1][1]] = "B"
+                if self.check(chess):
+                    return "B"
+            if i == 8:
+                return "Draw"
+        return "Pending"
+
+
+# @lc code=end
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.tictactoe([[0, 0], [2, 0], [1, 1], [2, 1], [2, 2]]))
+    print(s.tictactoe([[0, 0], [1, 1], [0, 1], [0, 2], [1, 0], [2, 0]]))
+    print(s.tictactoe([[0, 0], [1, 1], [2, 0], [1, 0],
+          [1, 2], [2, 1], [0, 1], [0, 2], [2, 2]]))
+    print(s.tictactoe([[0, 0], [1, 1]]))
---
+++ algorithm/1370.上升下降字符串.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: Deean
+Date: 2022-02-01 23:48:02
+LastEditTime: 2022-02-01 23:55:35
+Description: 
+FilePath: 1370.上升下降字符串.py
+'''
+#
+# @lc app=leetcode.cn id=1370 lang=python3
+#
+# [1370] 上升下降字符串
+#
+
+# @lc code=start
+from string import ascii_lowercase
+
+
+class Solution:
+    def sortString(self, s: str) -> str:
+        count = [0] * 26
+        for item in s:
+            count[ascii_lowercase.index(item)] += 1
+
+        s2 = ""
+        while len(s2) < len(s):
+            for i in range(26):
+                if count[i] > 0:
+                    s2 += ascii_lowercase[i]
+                    count[i] -= 1
+            for i in range(25, -1, -1):
+                if count[i] > 0:
+                    s2 += ascii_lowercase[i]
+                    count[i] -= 1
+        return s2
+# @lc code=end
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.sortString("aaaabbbbcccc"))
+    print(s.sortString("rat"))
+    print(s.sortString("leetcode"))
+    print(s.sortString("ggggggg"))
+    print(s.sortString("spo"))
