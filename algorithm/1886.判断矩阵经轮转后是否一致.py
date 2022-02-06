14bc5943775df4079ea52583dd71f5c762bb100f
spriteboysz <sprite_bx@126.com>
2022-02-06 23:36:38 +0800

    2022020623

---
+++ algorithm/1886.判断矩阵经轮转后是否一致.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: Deean
+Date: 2022-02-06 23:32:58
+LastEditTime: 2022-02-06 23:34:59
+Description: 
+FilePath: 1886.判断矩阵经轮转后是否一致.py
+'''
+#
+# @lc app=leetcode.cn id=1886 lang=python3
+#
+# [1886] 判断矩阵经轮转后是否一致
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
+        if mat == target:
+            return True
+        target = [tuple(row) for row in target]
+        for _ in range(3):
+            mat = list(zip(*mat))[::-1]
+            if mat == target:
+                return True
+        else:
+            return False
+
+# @lc code=end
