9be75b6785c05a52c2f182e3b797dae6d404645a
deean <sprite_bx@126.com>
2022-02-19 00:08:11 +0800

    2022021900

---
+++ algorithm/62.不同路径.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+# coding=utf-8
+"""
+Author: Deean
+Date: 2022-02-18 23:34:31
+LastEditTime: 2022-02-18 23:43:09
+Description: 
+FilePath: 62.不同路径.py
+"""
+#
+# @lc app=leetcode.cn id=62 lang=python3
+#
+# [62] 不同路径
+#
+
+# @lc code=start
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        grid = [[0] * n for _ in range(m)]
+        grid[0] = [1] * n
+        for i in range(m):
+            grid[i][0] = 1
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] = grid[i - 1][j] + grid[i][j - 1]
+        return grid[m - 1][n - 1]
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    solution = Solution()
+    ans = solution.uniquePaths(3, 7)
+    ans = solution.uniquePaths(3, 2)
+    ans = solution.uniquePaths(3, 3)
+    print(ans)
+    
---
+++ algorithm/8.字符串转换整数-atoi.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+# coding=utf-8
+"""
+Author: Deean
+Date: 2022-02-18 23:45:48
+LastEditTime: 2022-02-19 00:03:48
+Description: 
+FilePath: 8.字符串转换整数-atoi.py
+"""
+#
+# @lc app=leetcode.cn id=8 lang=python3
+#
+# [8] 字符串转换整数 (atoi)
+#
+
+# @lc code=start
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        if s.strip() == "":
+            return 0
+
+        s = s.strip().split()[0]
+        negative, num = False, 0
+        if s[0] == "-":
+            negative = True
+            s = s[1:]
+        elif s[0] == "+":
+            s = s[1:]
+        for item in s:
+            if item.isdigit():
+                num = num * 10 + int(item)
+                if num > 2 ** 32:
+                    num = 2 ** 32
+                    break
+            else:
+                break
+        if negative:
+            num = -num
+        if num >= 2 ** 31 - 1:
+            return 2 ** 31 - 1
+        elif num <= -(2 ** 31):
+            return -(2 ** 31)
+        else:
+            return num
+
+
+# @lc code=end
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ans = solution.myAtoi("")
+    ans = solution.myAtoi("   ")
+    print(ans)
