9f6706f8a866babc9367ff12bca879cbe8df66bd
deean <sprite_bx@126.com>
2022-03-02 23:04:39 +0800

    2022030223

--- algorithm/2.两数相加.py
+++ algorithm/2.两数相加.py
@@ -1,3 +1,12 @@
+#! /usr/bin/env python
+# coding=utf-8
+"""
+Author: Deean
+Date: 2022-02-16 00:07:56
+LastEditTime: 2022-03-02 22:53:42
+Description: 
+FilePath: 2.两数相加.py
+"""
 #
 # @lc app=leetcode.cn id=2 lang=python3
 #
@@ -6,11 +15,29 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-# @lc code=end
+        l3 = cur = ListNode()
+        carry = 0
+        while l1 or l2:
+            a = l1.val if l1 else 0
+            b = l2.val if l2 else 0
+            carry, mod = divmod(a + b + carry, 10)
+            cur.next = ListNode(mod)
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+            cur = cur.next
+        if carry:
+            cur.next = ListNode(carry)
+        return l3.next
 
+
+# @lc code=end
---
+++ algorithm/876.链表的中间结点.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# coding=utf-8
+"""
+Author: Deean
+Date: 2022-03-02 22:29:44
+LastEditTime: 2022-03-02 22:31:33
+Description: 
+FilePath: 876.链表的中间结点.py
+"""
+#
+# @lc app=leetcode.cn id=876 lang=python3
+#
+# [876] 链表的中间结点
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        length = 0
+        cur, node = head, head
+        while cur:
+            length += 1
+            cur = cur.next
+
+        length = length // 2
+        while length:
+            length -= 1
+            node = node.next
+        return node
+
+
+# @lc code=end
