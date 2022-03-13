8263ea4ff375ffca81827d4b3e9d9f63458bf695
deean <sprite_bx@126.com>
2022-03-13 22:50:36 +0800

    2022031322

---
+++ algorithm/1743.从相邻元素对还原数组.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+# coding=utf-8
+"""
+Author: Deean
+Date: 2022-03-12 22:12:42
+LastEditTime: 2022-03-12 22:37:26
+Description: 
+FilePath: 1743.从相邻元素对还原数组.py
+"""
+#
+# @lc app=leetcode.cn id=1743 lang=python3
+#
+# [1743] 从相邻元素对还原数组
+#
+
+# @lc code=start
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
+        adj = defaultdict(list)
+        for pair in adjacentPairs:
+            adj[pair[0]].append(pair[1])
+            adj[pair[1]].append(pair[0])
+
+        head = min(adj.items(), key=lambda el: len(el[1]))
+        array, cur = [head[0]], head[1][0]
+        while True:
+            array.append(cur)
+            if len(array) > len(adjacentPairs):
+                break
+            adj[cur].remove(array[-2])
+            cur = adj[cur][0]
+        return array
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    solution = Solution()
+    ans = solution.restoreArray([[2, 1], [3, 4], [3, 2]])
+    print(ans)
+    ans = solution.restoreArray([[4, -2], [1, 4], [-3, 1]])
+    print(ans)
+    ans = solution.restoreArray([[100000, -100000]])
+    print(ans)
---
+++ algorithm/729.我的日程安排表-i.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# coding=utf-8
+"""
+Author: Deean
+Date: 2022-03-12 23:11:34
+LastEditTime: 2022-03-12 23:11:58
+Description: 
+FilePath: 729.我的日程安排表-i.py
+"""
+#
+# @lc app=leetcode.cn id=729 lang=python3
+#
+# [729] 我的日程安排表 I
+#
+
+# @lc code=start
+class MyCalendar:
+    def __init__(self):
+        self.calendars = []
+
+    def book(self, start: int, end: int) -> bool:
+        for i, calendar in enumerate(self.calendars):
+            if end <= calendar[0]:
+                self.calendars.insert(i, (start, end))
+                break
+            elif end > calendar[0] and start < calendar[1]:
+                return False
+        else:
+            self.calendars.append((start, end))
+        return True
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
+# @lc code=end
