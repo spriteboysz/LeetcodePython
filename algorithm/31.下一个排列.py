78186e1703181bbd6bcf28161ed3277dc8576cc4
spriteboysz <sprit_bx@126.com>
2022-04-12 23:07:33 +0800

    2022041223

---
+++ algorithm/31.下一个排列.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# coding=utf-8
+'''
+Author: Deean
+Date: 2022-04-12 23:06:56
+LastEditTime: 2022-04-12 23:07:17
+Description: 
+FilePath: 31.下一个排列.py
+'''
+#
+# @lc app=leetcode.cn id=31 lang=python3
+#
+# [31] 下一个排列
+#
+
+# @lc code=start
+from typing import List
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        pass
+# @lc code=end
+
