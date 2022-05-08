e212e476fa24adc74a6cb980b88f6cf168e8918e
spriteboysz <sprite_bx@126.com>
2022-05-08 16:24:09 +0800

    2022050816

---
+++ 面试题/面试题 02.01. 移除重复节点.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def removeDuplicateNodes(self, head: ListNode) -> ListNode:
+        if not head:
+            return
+        nodes, values = [],[]
+        while head:
+            if head.val not in values:
+                nodes.append(head)
+                values.append(head.val)
+            head = head.next
+        
+        for i in range(len(nodes) - 1):
+            nodes[i].next = nodes[i + 1]
+        nodes[-1].next = None
+
+        return nodes[0]
---
+++ 面试题/面试题 02.02. 返回倒数第 k 个节点.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def kthToLast(self, head: ListNode, k: int) -> int:
+        slow, fast = head, head
+        for _ in range(k):
+            fast = fast.next
+        
+        while fast:
+            slow, fast = slow.next, fast.next
+        return slow.val
