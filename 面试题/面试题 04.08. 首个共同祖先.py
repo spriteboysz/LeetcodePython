69220da2d06efcf35e414588c2013ce10896aec2
spriteboysz <sprite_bx@126.com>
2022-06-11 16:36:29 +0800

    2022061116

---
+++ 面试题/面试题 04.08. 首个共同祖先.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+# coding=utf-8
+"""
+Author: Deean
+Date: 2022-06-11 16:21
+LastEditTime: 2022-06-11 16:21
+Description:
+FilePath: 面试题 04.08. 首个共同祖先.py
+"""
+
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(
+            self,
+            root: TreeNode,
+            p: TreeNode,
+            q: TreeNode) -> TreeNode:
+        def dfs(node, p, q):
+            if not node:
+                return
+            elif node == p or node == q:
+                return node
+            left = dfs(node.left, p, q)
+            right = dfs(node.right, p, q)
+
+            if left and right:
+                return node
+            elif left:
+                return left
+            elif right:
+                return right
+
+        return dfs(root, p, q)
---
+++ 面试题/面试题 17.08. 马戏团人塔.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# coding=utf-8
+"""
+Author: Deean
+Date: 2022-06-11 16:09
+LastEditTime: 2022-06-11 16:09
+Description:
+FilePath: 面试题 17.08. 马戏团人塔.py
+"""
+
+from bisect import bisect_left
+from typing import List
+
+class Solution:
+    def bestSeqAtIndex(self, height: List[int], weight: List[int]) -> int:
+        dp = []
+        for h, w in sorted(zip(height, weight),
+                           key=lambda el: (el[0], -el[1])):
+            position = bisect_left(dp, w)
+            dp[position:position + 1] = [w]
+            # print(dp)
+        return len(dp)
+
+if __name__ == '__main__':
+    solution = Solution()
+    ans = solution.bestSeqAtIndex(
+        height=[
+            65, 70, 56, 75, 60, 68], weight=[
+            100, 150, 90, 190, 95, 110])
+    print(ans)
