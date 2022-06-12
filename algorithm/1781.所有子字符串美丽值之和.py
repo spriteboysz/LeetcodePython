e1d4fcfcbe2a96af968ca0b2e9681ad0b4b05bab
spriteboysz <sprite_bx@126.com>
2022-06-12 16:21:01 +0800

    2022061216

---
+++ algorithm/1781.所有子字符串美丽值之和.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# coding=utf-8
+"""
+Author: Deean
+Date: 2022-06-12 15:55
+LastEditTime: 2022-06-12 15:55
+Description:
+FilePath: 1781.所有子字符串美丽值之和.py
+"""
+
+from collections import defaultdict
+
+
+class Solution:
+    def beautySum(self, s: str) -> int:
+        beauty = 0
+        for i in range(len(s)):
+            dic = defaultdict(int)
+            for j in range(i, len(s)):
+                dic[s[j]] += 1
+                beauty += max(dic.values()) - min(dic.values())
+        return beauty
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    ans = solution.beautySum("aabcb")
+    print(ans)
+    ans = solution.beautySum(s = "aabcbaa")
+    print(ans)
---
+++ algorithm/1785.构成特定和需要添加的最少元素.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# coding=utf-8
+"""
+Author: Deean
+Date: 2022-06-12 16:16
+LastEditTime: 2022-06-12 16:16
+Description:
+FilePath: 1785.构成特定和需要添加的最少元素.py
+"""
+
+from math import ceil
+from typing import List
+
+class Solution:
+    def minElements(self, nums: List[int], limit: int, goal: int) -> int:
+        return ceil(abs(goal - sum(nums)) / limit)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    ans = solution.minElements(nums=[1, -1, 1], limit=3, goal=-4)
+    print(ans)
+    ans = solution.minElements(nums=[1, -10, 9, 1], limit=100, goal=0)
+    print(ans)
