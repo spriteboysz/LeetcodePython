d5c8924966e1a4d5bc41ab8fc6b79aa19fa3e66c
spriteboysz <sprite_bx@126.com>
2022-06-18 23:12:25 +0800

    2022061823

---
+++ 剑指Offer/剑指 Offer 07. 重建二叉树.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# coding=utf-8
+"""
+Author: Deean
+Date: 2022-06-18 23:07
+LastEditTime: 2022-06-18 23:07
+Description:
+FilePath: 剑指 Offer 07. 重建二叉树.py
+"""
+
+from typing import List
+
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if not preorder:
+            return
+        root = TreeNode(preorder[0])
+        index = inorder.index(root.val)
+        root.left = self.buildTree(preorder[1: index + 1], inorder[0: index])
+        root.right = self.buildTree(preorder[index + 1:], inorder[index + 1:])
+        return root
