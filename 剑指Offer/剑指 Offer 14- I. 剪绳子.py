4ef272e75cf5cc9d2cfa7fa8b57b05cdb33eaac4
spriteboysz <sprite_bx@126.com>
2023-07-08 22:06:49 +0800

    2023070822

--- 剑指Offer/剑指 Offer 14- I. 剪绳子.py
+++ 剑指Offer/剑指 Offer 14- I. 剪绳子.py
@@ -11,7 +11,7 @@ FilePath: 剑指 Offer 14- I. 剪绳子
 
 class Solution:
     def cuttingRope(self, n: int) -> int:
-        dp = [0 for i in range(n + 2)]
+        dp = [0 for _ in range(n + 2)]
         i = 0
         for i in range(2, n + 1):
             cur = 0
