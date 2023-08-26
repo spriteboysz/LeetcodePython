60acc4c249f5db0d67a6e51f36970e38080790ae
spriteboysz <sprite_bx@126.com>
2023-08-26 22:59:13 +0800

    2023082622

--- common/MySQL.py
+++ common/MySQL.py
@@ -9,16 +9,23 @@ Description:MySQL.py
 import pandas as pd
 from sqlalchemy import create_engine
 
-HOST, PORT = 'localhost', '3306'
-USER, PASSWORD = 'root', '123456'
+# 定义数据库连接参数
+__HOST__ = 'localhost'
+__PORT__ = '3306'
+__USER__ = 'root'
+__PWD__ = '123456'
+
+
+def get_engine(db='p1757'):
+    url = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(
+        __USER__, __PWD__, __HOST__, __PORT__, db)
+    return create_engine(url)
 
 
 def get_sql_table(db='p1757', table='products'):
-    url = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USER, PASSWORD, HOST, PORT, db)
-    engine = create_engine(url)
+    engine = get_engine(db)
     with engine.connect() as conn:
-        products = pd.read_sql_table(table, conn)
-        return products
+        return pd.read_sql_table(table, conn)
 
 
 if __name__ == '__main__':
