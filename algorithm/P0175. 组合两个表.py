e85282a45f01f1f9037d6f99097363ab83e517df
spriteboysz <sprite_bx@126.com>
2023-08-30 22:23:21 +0800

    2023083022

--- algorithm/P0175. 组合两个表.py
+++ algorithm/P0175. 组合两个表.py
@@ -20,3 +20,4 @@ if __name__ == '__main__':
     person = get_sql_table('P0175', 'person')
     address = get_sql_table('P0175', 'address')
     print(combine_two_tables(person, address))
+    print()
