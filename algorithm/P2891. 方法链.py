376909ddf728a19e92c1808328499ffcc564d61d
spriteboysz <sprite_bx@126.com>
2024-01-10 00:06:53 +0800

    2024011000

---
+++ algorithm/P2891. 方法链.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# coding=utf-8
+"""
+Author: Deean
+Date: 2024-01-09 23:20
+FileName: algorithm
+Description:P2891. 方法链.py 
+"""
+
+import pandas as pd
+from common.leetcode import ss_to_pd
+
+
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    return animals[animals['weight'] > 100].sort_values(by='weight', ascending=False)[['name']]
+
+
+if __name__ == '__main__':
+    animals = ss_to_pd("""
++----------+---------+-----+--------+
+| name     | species | age | weight |
++----------+---------+-----+--------+
+| Tatiana  | Snake   | 98  | 464    |
+| Khaled   | Giraffe | 50  | 41     |
+| Alex     | Leopard | 6   | 328    |
+| Jonathan | Monkey  | 45  | 463    |
+| Stefan   | Bear    | 100 | 50     |
+| Tommy    | Panda   | 26  | 349    |
++----------+---------+-----+--------+
+    """)
+    print(findHeavyAnimals(animals))
--- common/leetcode.py
+++ common/leetcode.py
@@ -7,4 +7,30 @@ FileName: common/leetcode.py
 Description: 
 """
 
+import pandas as pd
 
+
+def ss_to_pd(ss):
+    col, grid = [], []
+    for line in filter(lambda line: line.strip().startswith("|"), ss.splitlines()):
+        row = list(map(lambda word: word.strip(), line.strip("|").split("|")))
+        if len(col) == 0:
+            col = row
+        else:
+            grid.append(map(lambda el: int(el) if el.isdigit() else el, row))
+    return pd.DataFrame(grid, columns=col)
+
+
+if __name__ == '__main__':
+    pd = ss_to_pd("""
++----------+---------+-----+--------+
+| name     | species | age | weight |
++----------+---------+-----+--------+
+| Tatiana  | Snake   | 98  | 464    |
+| Khaled   | Giraffe | 50  | 41     |
+| Alex     | Leopard | 6   | 328    |
+| Jonathan | Monkey  | 45  | 463    |
+| Stefan   | Bear    | 100 | 50     |
+| Tommy    | Panda   | 26  | 349    |
++----------+---------+-----+--------+""")
+    print(pd)
