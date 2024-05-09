2b8bc0a0104e2b238cc0838aadcd0c2a44d5358c
spriteboysz <sprite_bx@126.com>
2024-05-09 23:22:25 +0800

    2024050923

--- algorithm/P0763. 划分字母区间.py
+++ algorithm/P0763. 划分字母区间.py
@@ -11,7 +11,17 @@ from typing import List
 
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        pass
+        last = [0] * 26
+        for i, ch in enumerate(s):
+            last[ord(ch) - ord('a')] = i
+        partition = []
+        start, end = 0, 0
+        for i, ch in enumerate(s):
+            end = max(end, last[ord(ch) - ord('a')])
+            if i == end:
+                partition.append(end - start + 1)
+                start = end + 1
+        return partition
 
 
 if __name__ == '__main__':
