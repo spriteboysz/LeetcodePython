7c86543e69a15f6fee55da4a4ab3e14758b4c13e
spriteboysz <sprite_bx@126.com>
2024-05-10 22:53:22 +0800

    2024051022

---
+++ algorithm/P0406. 根据身高重建队列.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# coding=utf-8
+"""
+Author: Deean
+Date: 2024-05-09 23:47
+FileName: algorithm
+Description:P0406. 根据身高重建队列.py 
+"""
+from typing import List
+
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        pass
+
+
+if __name__ == '__main__':
+    print(Solution().reconstructQueue(people=[[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]))
